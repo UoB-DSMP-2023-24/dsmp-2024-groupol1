8c7595cf363a540f30873e1118994c0d7257b494
joesharratt1229 <joesharratt1229@gmail.com>
2024-03-04 08:40:13 +0000

    reformatted tsne vis

--- preprocess.py
+++ preprocess.py
@@ -7,14 +7,13 @@ def get_alpha_beta_chains(df, column_name):
     df[f'{column_name}_a'] = df[column_name].apply(lambda x: x.split(';')[0])
     df[f'{column_name}_b'] = df[column_name].apply(lambda x: x.split(';')[1])
     df = df.drop(columns = column_name)
-    print('hi')
     return df
 
 def preprocess_data(file_path: str):
     data = pd.read_csv(file_path, sep = '\t')
-    data.drop(columns = ['antigen.gene', 'reference.id', 'method', 'meta', 'cdr3fix', 'web.method', 'web.method.seq', 'web.cdr3fix.nc', 'web.cdr3fix.unmp'], inplace = True)
-    non_transformed_cols = ['complex.id', 'species', 'mhc.a', 'mhc.b', 'mhc.class', 'antigen.epitope', 'antigen.species', 'vdjdb.score']
+    data = data[['gene','cdr3','v.segm','j.segm','species','mhc.a','mhc.b','mhc.class','antigen.epitope','antigen.species','vdjdb.score']]
     data = data.sort_values(by = 'gene')
+    non_transformed_cols = ['complex.id', 'species', 'mhc.a', 'mhc.b', 'mhc.class', 'antigen.epitope', 'antigen.species', 'vdjdb.score']
     agg_dict = {col: 'first' for col in non_transformed_cols}
     agg_dict.update({col: concatenate_rows for col in data.columns if col not in non_transformed_cols})
 
